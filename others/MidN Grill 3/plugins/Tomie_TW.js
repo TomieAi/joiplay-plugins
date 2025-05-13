64bba7ac3afe4b6acc555efeb1fe187763cae7ca
Tomie <111332073+TomieAi@users.noreply.github.com>
2025-05-14 03:18:13 +0800

    updated

--- others/MidN Grill 3/plugins/Tomie_TW.js
+++ others/MidN Grill 3/plugins/Tomie_TW.js
@@ -1,35 +1,18 @@
 /*
 @about This plugin is for MidN Grill 3 only
 @author TomieAi
-@version 1.0
+@version 1.2
 
 This game uses old version of node version 3.x.x i hate this xD its so messy cant use some api on modern node.
 */
-function TomieTextWrapper(text, name, bitmap) {
+var TomiePluginParams = PluginManager.parameters('Tomie_TW');
+var TomiePluginConfig = TomiePluginParams.config || [];
+function TomieTextWrapper(text, name, bitmap, contentsWidth) {
     this.m_text = text;
     this.m_name = name;
     this.m_bitmap = bitmap;
-}
-TomieTextWrapper.prototype.config = function () {
-    return [
-        { name: 'テキスト枠　セリフ風１－１', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風１－２', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風２－１', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風２－２', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風２－２下', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風３－１', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風３－２', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風３－３', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風３－４', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風４－１', maxWidth: 350 },
-        { name: 'テキスト枠　セリフ風４－２', maxWidth: 350 },
-        { name: 'テキスト枠　衣装選択用', maxWidth: 900 },
-        { name: 'テキスト枠　衣装選択用　旧', maxWidth: 900 },
-        { name: 'テキスト枠　誘惑露出　相手右　男', maxWidth: 350 },
-        { name: 'テキスト枠　誘惑露出　相手右　男２', maxWidth: 350 },
-        { name: 'テキスト枠　通常会話用', maxWidth: 1250 },
-        { name: 'テキスト枠　露出用', maxWidth: 350 }
-    ]
+    this.m_widthCache = {};
+    this.m_contentsWidth = contentsWidth;
 }
 TomieTextWrapper.prototype.splitSentences = function (text) {
     var parts = [];
@@ -50,25 +33,24 @@ TomieTextWrapper.prototype.splitSentences = function (text) {
     }
     return parts;
 }
-TomieTextWrapper.prototype.measure = function (text, widthCache) {
-    if (widthCache[text] !== undefined) {
-        return widthCache[text];
+TomieTextWrapper.prototype.measure = function (text) {
+    if (this.m_widthCache[text] !== undefined) {
+        return this.m_widthCache[text];
     }
     var w = this.m_bitmap.measureTextWidth(text);
-    widthCache[text] = w;
+    this.m_widthCache[text] = w;
     return w;
 }
 TomieTextWrapper.prototype.getMaxWidthByName = function (name) {
-    for (var i = 0; i < this.config().length; i++) {
-        if (this.config()[i].name === name) return this.config()[i].maxWidth;
+    for (var i = 0; i < TomiePluginConfig.length; i++) {
+        if (TomiePluginConfig[i].name === name) return TomiePluginConfig[i].maxWidth;
     }
     return null;
 }
 TomieTextWrapper.prototype.wrap = function () {
     var text = this.m_text;
-    var maxWidth = this.getMaxWidthByName(this.m_name);
+    var maxWidth = TomiePluginParams.vanilla ? this.m_contentsWidth : this.getMaxWidthByName(this.m_name);
     if (!this.m_bitmap || !this.m_bitmap.measureTextWidth || !maxWidth) return text;
-    var widthCache = {};
     const normalized = text
         .replace(/\n/g, ' ')
         .replace(/\s+/g, ' ')
@@ -92,13 +74,13 @@ TomieTextWrapper.prototype.wrap = function () {
 
         if (currentLine) {
             var testLine = currentLine + ' ' + sentence;
-            if (this.measure(testLine, widthCache) <= maxWidth) {
+            if (this.measure(testLine) <= maxWidth) {
                 currentLine = testLine;
                 continue;
             }
         }
 
-        if (this.measure(sentence,widthCache) <= maxWidth) {
+        if (this.measure(sentence) <= maxWidth) {
             if (currentLine) lines.push(currentLine);
             currentLine = sentence;
             continue;
@@ -107,12 +89,12 @@ TomieTextWrapper.prototype.wrap = function () {
         for (var j = 0; j < words.length; j++) {
             var word = words[j];
             var testLine = currentLine ? currentLine + ' ' + word : word;
-            var width = this.measure(testLine,widthCache);
+            var width = this.measure(testLine);
 
             if (width <= maxWidth) {
                 currentLine = testLine;
             } else {
-                if (currentLine === '' || this.measure(word,widthCache) > maxWidth) {
+                if (currentLine === '' || this.measure(word) > maxWidth) {
                     if (currentLine) {
                         lines.push(currentLine);
                         currentLine = '';
@@ -122,7 +104,7 @@ TomieTextWrapper.prototype.wrap = function () {
                     for (var k = 0; k < word.length; k++) {
                         var char = word[k];
                         var testChar = currentWordPart + char;
-                        if (this.measure(testChar,widthCache) <= maxWidth) {
+                        if (this.measure(testChar) <= maxWidth) {
                             currentWordPart = testChar;
                         } else {
                             lines.push(currentWordPart);
@@ -147,41 +129,41 @@ TomieTextWrapper.prototype.wrap = function () {
     return lines.join('\n');
 }
 
-var bitmap = null;
+var bitmap_bg = null;
 var message_background = "";
-
 Window_Message.prototype.updateBackground = function () {
     this._background = $gameMessage.background();
     this.setBackgroundType(this._background);
-    if (this.contents) bitmap = this.contents;
+    if (this.contents) bitmap_bg = this.contents;
 }
 var o_sprite_onbitmapload = Sprite.prototype._onBitmapLoad;
-
 Sprite.prototype._onBitmapLoad = function (bitmapLoaded) {
     o_sprite_onbitmapload.call(this, bitmapLoaded);
-    if (!bitmap) bitmap = bitmapLoaded;
+    if (!bitmap_bg) bitmap_bg = bitmapLoaded;
 }
 Window_Base.prototype.standardFontSize = function () {
-    return 18;
+    return TomiePluginParams.fontSize || 18;
 };
 Window_Base.prototype.standardPadding = function () {
-    return 18;
+    return TomiePluginParams.padding || 18;
 };
 Window_Message.prototype.startMessage = function () {
     this._textState = {};
     this._textState.index = 0;
-    this._textState.text = new TomieTextWrapper(this.convertEscapeCharacters($gameMessage.allText()), message_background, bitmap).wrap();
+    this._textState.text = new TomieTextWrapper(this.convertEscapeCharacters($gameMessage.allText()), message_background, bitmap_bg, this.contentsWidth()).wrap();
     this.newPage(this._textState);
     this.updatePlacement();
     this.updateBackground();
     this.open();
 }
-
 var original_command231 = Game_Interpreter.prototype.command231;
 Game_Interpreter.prototype.command231 = function () {
     original_command231.call(this);
-    if (this._params[1].includes("テキスト枠")) {
-        message_background = this._params[1];
+    for(var i = 0; i < TomiePluginConfig.length; i++) {
+        if (TomiePluginConfig[i].name === this._params[1]) {
+            message_background = this._params[1];
+            break;
+        }
     }
     return true;
 }
